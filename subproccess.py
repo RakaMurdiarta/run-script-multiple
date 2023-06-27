e651b47f2f9bcc16588813c16e6999929b0998c8
RakaMurdiarta <dewamurdiarta@gmail.com>
2023-06-27 22:42:40 +0800

    while true way

--- subproccess.py
+++ subproccess.py
@@ -4,70 +4,57 @@ import json
 import subprocess
 
 root_directory = "C:\\Users\\Djoin\\Documents\\djoin-job-desk\\etl-core\\scripts"
-
-all_list = []
-
-
-def get_files_in_directory(rootdir, folder):
-    join_folder = os.path.join(rootdir, folder)
-    for file_path in glob.glob(os.path.join(join_folder, "*"), recursive=True):
-        if os.path.isfile(file_path):
-            all_list.append(file_path)
+root = r"C:\Users\Djoin\Documents\djoin-job-desk\run-script-multiple\run"
 
 
-def run_sub(list_data_run):
-    for run_py in list_data_run:
-        try:
-            subprocess.run(["python", run_py], check=True)
-            monitoring_run[run_py] = "[SUCCESS]"
-        except Exception as e:
-            print(f"there is an error {str(e)}")
-            monitoring_run[run_py] = f"[FAILED] : {str(e)}"
+def get_files_in_directory(root_path):
+    dir_list = []
+    all_path = []
+    for root, dirs, files in os.walk(root_path):
+        dir_list.append(root)
 
+    for dir_ in dir_list:
+        for filename in os.listdir(dir_):
+            if filename.endswith(".py"):
+                all_path.append(os.path.join(dir_, filename))
+
+    return all_path
+
+
+def run_sub(run_py, file_monitoring):
+    try:
+        subprocess.run(["python", run_py], check=True)
+        file_monitoring[run_py] = "[SUCCESS]"
+    except Exception as e:
+        print(f"there is an error {str(e)}")
+        file_monitoring[run_py] = f"[FAILED] : {str(e)}"
+
+    print('create file')
     with open("monitoring.json", "w") as outfile:
-        json.dump(monitoring_run, outfile)
+        json.dump(file_monitoring, outfile)
 
 
-list_sub_folder = [
-    "accounting",
-    "admin-log-operation",
-    "bank-transfer",
-    "deposit",
-    "employee",
-    "loan",
-    "log-operation",
-    "members",
-    "notifikasi",
-    "ppob",
-    "saving",
-]
+all_run_sub = get_files_in_directory(root_path=root)
+retries = 0
+while True:
+    data = []
+    filtering_run_sub = []
+    try:
+        with open("./monitoring.json") as f:
+            monitoring = json.load(f)
+    except Exception as e:
+        monitoring = {}
 
-for sub_list in list_sub_folder:
-    get_files_in_directory(root_directory, sub_list)
+    for path in all_run_sub:
+        try:
+            if monitoring[path] != "[SUCCESS]":
+                filtering_run_sub.append(path)
+        except:
+            filtering_run_sub.append(path)
 
-try:
-    with open("./monitoring.json") as f:
-        monitoring = json.load(f)
-except:
-    monitoring = {}
-
-
-monitoring_run = {}
-if len(monitoring) == 0:
-    run_sub(all_list)
-else:
-    get_list_failed = [key for key in monitoring.keys() if monitoring[key] != "[SUCCESS]"]
-    if len(get_list_failed) == 0:
-        print("all running success")
-        os.remove("monitoring.json")
-    else:
-        run_sub(get_list_failed)
-
-
-data = []
-try:
-    with open("./monitoring.json") as f:
-        monitoring = json.load(f)
+    print(filtering_run_sub)
+    for run_py in filtering_run_sub:
+        run_sub(run_py=run_py, file_monitoring=monitoring)
 
     for kunci in monitoring.keys():
         if monitoring[kunci] == "[SUCCESS]":
@@ -77,8 +64,9 @@ try:
 
     if all(data) == True:
         os.remove("monitoring.json")
+        break
 
-    print(data)
+    if retries == 3:
+        break
 
-except:
-    print("file not exist")
+    retries += 1
